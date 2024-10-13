a9391a3f47105389f4c6ded4dbd46a02d766e176
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-13 15:43:19 -0300

    feat: Update project timer and project proposals

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -10,64 +10,64 @@ use Livewire\Component;
 
 class Create extends Component
 {
-    public Project $project;
+  public Project $project;
 
-    public bool $modal = false;
+  public bool $modal = false;
 
-    #[Validate(['required', 'email'])]
-    public string $email = '';
+  #[Validate(['required', 'email'])]
+  public string $email = '';
 
-    #[Rule(['required', 'numeric', 'gt:0'])]
-    public int $hours = 0;
+  #[Rule(['required', 'numeric', 'gt:0'])]
+  public int $hours = 0;
 
-    public bool $agree = false;
+  public bool $agree = false;
 
-    public function save()
-    {
-        $this->validate();
+  public function save()
+  {
+    $this->validate();
 
-        if (!$this->agree) {
-            $this->addError('agree', 'Você precisa concordar com os termos de uso');
+    if (!$this->agree) {
+      $this->addError('agree', 'Você precisa concordar com os termos de uso');
 
-            return;
-        }
-
-        DB::transaction(function () {
-            $proposal = $this->project->proposals()
-                ->updateOrCreate(
-                    ['email' => $this->email],
-                    ['hours' => $this->hours]
-                );
-            // $this->arrangePositions($proposal);
-        });
-
-        // $this->project->author->notify(new NewProposal($this->project));
-
-        // $this->dispatch('proposal::created');
-        $this->modal = false;
+      return;
     }
 
-    // public function arrangePositions(Proposal $proposal)
-    // {
-    //     $query = DB::select('
-    //         select *, row_number() over (order by hours asc) as newPosition
-    //         from proposals
-    //         where project_id = :project
-    //         ', ['project' => $proposal->project_id]);
-    //     $position = collect($query)->where('id', '=', $proposal->id)->first();
-    //     $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
-    //     if ($otherProposal) {
-    //         $proposal->update(['position_status' => 'up']);
-    //         $oProposal = Proposal::find($otherProposal->id);
+    DB::transaction(function () {
+      $proposal = $this->project->proposals()
+        ->updateOrCreate(
+          ['email' => $this->email],
+          ['hours' => $this->hours]
+        );
+      // $this->arrangePositions($proposal);
+    });
 
-    //         $oProposal->update(['position_status' => 'down']);
-    //         $oProposal->notify(new PerdeuMane($this->project));
-    //     }
-    //     ArrangePositions::run($proposal->project_id);
-    // }
+    // $this->project->author->notify(new NewProposal($this->project));
 
-    public function render()
-    {
-        return view('livewire.proposals.create');
-    }
+    $this->dispatch('proposal::created');
+    $this->modal = false;
+  }
+
+  // public function arrangePositions(Proposal $proposal)
+  // {
+  //     $query = DB::select('
+  //         select *, row_number() over (order by hours asc) as newPosition
+  //         from proposals
+  //         where project_id = :project
+  //         ', ['project' => $proposal->project_id]);
+  //     $position = collect($query)->where('id', '=', $proposal->id)->first();
+  //     $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
+  //     if ($otherProposal) {
+  //         $proposal->update(['position_status' => 'up']);
+  //         $oProposal = Proposal::find($otherProposal->id);
+
+  //         $oProposal->update(['position_status' => 'down']);
+  //         $oProposal->notify(new PerdeuMane($this->project));
+  //     }
+  //     ArrangePositions::run($proposal->project_id);
+  // }
+
+  public function render()
+  {
+    return view('livewire.proposals.create');
+  }
 }
